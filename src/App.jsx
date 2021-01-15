44bc9c8fd78816996f8cc2da8bd8721ae6c5e5e1
Nerond32 <nerond32@gmail.com>
2021-01-15 21:33:02 +0100

    Initial cleanup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
